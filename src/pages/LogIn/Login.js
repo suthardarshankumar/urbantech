3a51aa19764dad5f115b95a7f1179d95e0890222
suthardarshankumar <suthardarshan2782@gmail.com>
2024-05-17 13:26:47 +0530

    Update Code

--- src/pages/LogIn/Login.js
+++ src/pages/LogIn/Login.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import { FaGoogle } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 import styles from './LogIn.module.css';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const storedPassword = localStorage.getItem('password');
+        if (password === storedPassword) {
+            alert('Login successful');
+        } else {
+            alert('Invalid password');
+        }
+    }
+
     return (
         <>
             <section className={styles.section}>
                 <div className={styles.formContain}>
-                    <form className={styles.form}>
+                    <form className={styles.form} onSubmit={handleLogin}>
                         <div className={styles.headingSection}>
                             <p className={styles.title}>LogIn</p>
                         </div>
                         <div className={styles.labelSection}>
                             <div className={styles.labels}>
-                                <input className={styles.input} type='email' placeholder='E-Mail' />
+                                <input
+                                    className={styles.input}
+                                    type='email'
+                                    placeholder='E-Mail'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
                             </div>
                             <div className={styles.labels}>
-                                <input className={styles.input} type='password' placeholder='Password' />
+                                <input
+                                    className={styles.input}
+                                    type='password'
+                                    placeholder='Password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                             </div>
                             <div className={styles.forgetPassword}>
                                 <Link to='forget-password' className={styles.forgetPassword}>Forget Password</Link>
                             </div>
                         </div>
                         <div className={styles.btnSection}>
-                            <button className={styles.btn}>LogIn</button>
+                            <Link to='/'>   <button className={styles.btn} type='submit'>LogIn</button> </Link>
                         </div>
 
-                        <div className={styles.container}>
-                            <div className={styles.heading}>
-                                Or LogIn with
-                            </div>
+                        <div className={styles.container}> <div className={styles.heading}>
+                            Or LogIn with
+                        </div>
                             <div className={styles.iconsSection}>
                                 <div className={styles.iconContain}>
                                     <FaGoogle className={styles.icon} />
@@ -44,7 +66,7 @@ const Login = () => {
                     </form>
                     <div className={styles.signupContainer}>
                         <div className={styles.headingSign}>
-                            New at UrbanTech ?
+                            New at UrbanTech?
                         </div>
                         <div className={styles.signupbtn}>
                             <Link to='signUp'><button className={styles.signbtn}>SignUp</button></Link>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,35 +1,75 @@
-import React from 'react';
-import { FaGoogle } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
-
+import React, { useState } from 'react';
+import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 import styles from './SignUp.module.css';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            localStorage.setItem('password', password);
+            setFullName('');
+            setEmail('');
+            setPassword('')
+            setConfirmPassword('')
+        } else {
+
+        }
+    }
+
     return (
         <>
             <section className={styles.section}>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSignUp}>
                     <div className={styles.headingSection}>
                         <p className={styles.title}>Register</p>
                         <p className={styles.message}>Signup now and get full access to our app.</p>
                     </div>
                     <div className={styles.labelSection}>
                         <div className={styles.labels}>
-                            <input className={styles.input} type='text' placeholder='Full Name' />
+                            <input
+                                className={styles.input}
+                                type='text'
+                                placeholder='Full Name'
+                                value={fullName}
+                                onChange={(e) => setFullName(e.target.value)}
+                            />
                         </div>
                         <div className={styles.labels}>
-                            <input className={styles.input} type='email' placeholder='E-Mail' />
+                            <input
+                                className={styles.input}
+                                type='email'
+                                placeholder='E-Mail'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className={styles.labels}>
-                            <input className={styles.input} type='password' placeholder='Password' />
+                            <input
+                                className={styles.input}
+                                type='password'
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
                         <div className={styles.labels}>
-                            <input className={styles.input} type='password' placeholder='Confirm Password' />
+                            <input
+                                className={styles.input}
+                                type='password'
+                                placeholder='Confirm Password'
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
                         </div>
                     </div>
                     <div className={styles.btnSection}>
-                        <button className={styles.btn}>SignUp</button>
+                        <Link to='/logInPage'> <button className={styles.btn} type='submit'>SignUp</button></Link>
                     </div>
 
                     <div className={styles.container}>
